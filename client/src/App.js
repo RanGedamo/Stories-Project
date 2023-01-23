c7c063b40d358c94076b8f4ac774e2d6398d28c5
RanGedamo <mcrangedamo@gmail.com>
2023-01-23 18:01:00 +0200

    clear app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-import Avatar from "./components/avatar/Avatar";
-import StoriesCarousel from "./components/Carousel/StoriesCarousel";
 
 export default function App() {
   return (
   <div className="App">
-    <section className='carousel-status'>
-      <StoriesCarousel />
-    </section>
+
     </div>
     )
 };
