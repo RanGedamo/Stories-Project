87bb5155e4eecd855d8af7741689cbba03882758
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2023-01-24 00:49:31 +0200

    oo

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import MyCalendar from "./pages/Calender";
 
 import Routing from "./Routing";
 
 export default function App() {
   return (
-    <div className="">
+    <div className="App">
       <BrowserRouter>
         <Routing />
       </BrowserRouter>
-      <MyCalendar/>
-
-
     </div>
   )
 };
