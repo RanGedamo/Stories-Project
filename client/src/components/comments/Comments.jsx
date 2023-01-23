524e9b4ef1ad76c3b058b3115a7e04a11bd998d6
NetanelTesfay <netaneltes66@gmail.com>
2023-01-24 01:13:22 +0200

    story

---
+++ client/src/components/comments/Comments.jsx
@@ -0,0 +1,104 @@
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardFooter,
+  MDBCardImage,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBRow,
+  MDBTextArea,
+} from "mdb-react-ui-kit";
+
+export default function CommentCard() {
+  return (
+      <MDBContainer className="py-5" style={{ maxWidth: "1000px" }}>
+        <MDBRow className="justify-content-center">
+          <MDBCol md="12" lg="10" xl="8">
+            <MDBCard>
+              <MDBCardBody>
+                <div className="d-flex flex-start align-items-center">
+                  <MDBCardImage
+                    className="rounded-circle shadow-1-strong me-3"
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp"
+                    alt="avatar"
+                    width="60"
+                    height="60"
+                  />
+                  <div>
+                    <h6 className="fw-bold text-primary mb-1">Lily Coleman</h6>
+                    <p className="text-muted small mb-0">
+                      Shared publicly - Jan 2020
+                    </p>
+                  </div>
+                </div>
+
+                <span className="mt-3 mb-4 pb-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip consequat.
+                </span>
+              </MDBCardBody>
+              <MDBCardBody>
+                <div className="d-flex flex-start align-items-center">
+                  <MDBCardImage
+                    className="rounded-circle shadow-1-strong me-3"
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp"
+                    alt="avatar"
+                    width="60"
+                    height="60"
+                  />
+                  <div>
+                    <h6 className="fw-bold text-primary mb-1">Lily Coleman</h6>
+                    <p className="text-muted small mb-0">
+                      Shared publicly - Jan 2020
+                    </p>
+                  </div>
+                </div>
+
+                <span className="mt-3 mb-4 pb-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip consequat.
+                </span>
+              </MDBCardBody>
+              
+
+              <MDBCardFooter
+                className="py-3 border-0"
+                style={{ backgroundColor: "#f8f9fa" }}
+              >
+                <div className="d-flex flex-start w-100">
+                  <MDBCardImage
+                    className="rounded-circle shadow-1-strong me-3"
+                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp"
+                    alt="avatar"
+                    width="40"
+                    height="40"
+                  />
+                  <MDBTextArea
+                    label="Message"
+                    id="textAreaExample"
+                    rows={4}
+                    style={{ backgroundColor: "#fff" }}
+                    wrapperClass="w-100"
+                  />
+                </div>
+                <div className="float-end mt-2 pt-1">
+                  <MDBBtn size="sm" className="me-1">
+                    Post comment
+                  </MDBBtn>
+                  <MDBBtn outline size="sm">
+                    Cancel
+                  </MDBBtn>
+                </div>
+              </MDBCardFooter>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+  );
+}
