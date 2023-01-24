ad70bc72b5c98caaf1bfae2e9567dfaa36c613c2
RanGedamo <mcrangedamo@gmail.com>
2023-01-24 03:43:13 +0200

    ran

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -15,6 +15,7 @@ import axios from 'axios';
 
 function SignUp() {
   const [errInput,setErrInput] = useState("")
+  const [loading,setLoading] = useState(false)
   const [inputs, setInputs] = useState()
   const [image, setImage] = useState()
   const [userAvatar, setUserAvatar] = useState()
@@ -25,21 +26,26 @@ function SignUp() {
   }
 
   const submitAvatar = async () => {
+    setLoading(true)
     const formData = new FormData()
     formData.append("file", image)
     formData.append("folder", "avatar")
     formData.append("upload_preset", "avatar")
     await axios.post('https://api.cloudinary.com/v1_1/dcapisw77/image/upload', formData)
       .then(res => {
+        setLoading(true)
         if (res.data.url) {
           const data = res.data.url
           const myData = { ...inputs, avatar: data };
           return submitUser(myData)
         }
-      }).catch(err => setErrInput("image not found"))
+      }).catch(err => {
+        setErrInput("image not found")
+      })
   };
 
   const submitUser = async (data) => {
+    setLoading(false)
     return await registerUser(data).then(res => console.log(res))
       .catch(error =>{ 
         if(error.response.data.message)
@@ -63,8 +69,9 @@ function SignUp() {
                 <MDBInput wrapperClass='mb-4 mx-5 w-100' style={{ color: "white" }} labelClass='text-white' label='Confirm Password' id='formControlLg' type='password' size="lg" name='confirmPassword' onChange={(e) => changeInputs(e)} />
                 <MDBInput wrapperClass='mb-4 mx-5 w-100' style={{ color: "white" }} labelClass='text-white' label='Image' id='formControlLg' type='file' size="lg" onChange={(e) => setImage(e.target.files[0])} />
                
-             {errInput?<div style={{color:"red",fontSize:25,fontFamily:"-moz-initial",border:"1px red solid" ,width:"100%"}} >{errInput}</div>:""}
-               
+             {errInput?(<div style={{color:"red",fontSize:25,fontFamily:"-moz-initial",border:"1px red solid" ,width:"100%"}} >{errInput}</div>):loading?
+              <div style={{width:200,height:120}}><img src='https://thumbs.gfycat.com/AgitatedInexperiencedImperialeagle-max-1mb.gif' width={112} height={80}/></div>:""}
+            
                 <p className="small mb-3 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
                 <MDBBtn outline className='mx-2 px-5 text-white' color='white' size='lg' type='submit' onClick={() => submitAvatar()}>
                   Register
