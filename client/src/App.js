8e12d7a68eda654bf3c48bf0b0bb0f5d96fcf821
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2023-01-24 05:47:54 +0200

    f

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Comunnity from "./pages/Comunnity/Comunnity";
 import Routing from "./Routing";
 
 export default function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
       {/* <NavBar/> */}
-      <Routing/>
-      </BrowserRouter>
+      {/* <Routing/>
+      </BrowserRouter> */}
+<Comunnity/>
     </div>
   )
 };
---
+++ client/src/pages/Comunnity/Comunnity.jsx
@@ -0,0 +1,28 @@
+import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
+import React from "react";
+import Reels from "../../components/reels/reels";
+
+function Comunnity() {
+  return (
+    <MDBContainer>
+      <MDBRow style={{ background: "red", height: "20vh" }}>ggg</MDBRow>
+      <MDBRow style={{ background: "yellow", height: "100vh" }}>
+        <MDBCol
+          className="flex-column"
+          md={6}
+          style={{ background: "blue", height: "100vh" }}
+        >
+          <MDBCol md={6}>
+            <Reels />
+          </MDBCol>
+          <MDBCol md={6}>
+            
+          </MDBCol>
+        </MDBCol>
+        <MDBCol md={6} style={{ background: "blue", height: "100vh" }}></MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
+
+export default Comunnity;
