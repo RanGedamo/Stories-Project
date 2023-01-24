d268b06ea80685e452d20e2ea7e87e4b80742bca
chen-semu <chensemu@gmail.com>
2023-01-24 04:07:00 +0200

    added default avatar

--- server/models/users-model.js
+++ server/models/users-model.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const usersSchema=schema({
-    userName:{type:String, required:true},
-    email:{type:String, required:true},
-    password:{type:String,required:true},
-    groups:[{type:schema.Types.ObjectId,ref:"group"}],
-    avatar:{type:String,default:"",required:true}
-}, { timestamps: true })
+const usersSchema = schema(
+  {
+    userName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    groups: [{ type: schema.Types.ObjectId, ref: "group" }],
+    avatar: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dtcgejl11/image/upload/v1673277784/team_project_lawyers/gxi78yqf4eclrsncct3z.webp",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports=mongoose.model("user", usersSchema);
+module.exports = mongoose.model("user", usersSchema);
