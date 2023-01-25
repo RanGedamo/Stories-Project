e533391592d928b4e0896b5038a9f37935f89e63
chen-semu <chensemu@gmail.com>
2023-01-25 11:05:58 +0200

    adding drop down

--- server/router/user-router.js
+++ server/router/user-router.js
@@ -6,11 +6,13 @@ const {
     deleteUser,
     register,
     logIn,
+    getByEmail
 } = require("../controllers/users-ctrl");
 
 
 router.get("/", getAll);
 router.get("/byId/:id", getById);
+router.get("/findByEmail/:email", getByEmail);
 router.post("/logIn", logIn);
 router.post("/register", register);
 router.put("/update/byId/:id", update);
